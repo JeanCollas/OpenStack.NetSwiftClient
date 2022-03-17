968ef99c87c1c76d5e059919f6e0452b8b5e888c
Andriy S'omak <semack@gmail.com>
2022-03-17 15:38:19 +0200

    Given access to HttpClient property in SwiftClient

--- src/NSC/SwiftClientService.cs
+++ src/NSC/SwiftClientService.cs
@@ -16,31 +16,37 @@ namespace NetSwiftClient
         public SwiftClientConfig Config { get; set; }
         public string ObjectStoreUrl => Config?.ObjectStoreUrl;
         public string Token { get; set; }
+
         /// <summary>If no config is provided</summary>
         public SwiftClientService()
         {
         }
+
         public SwiftClientService(SwiftClientConfig config)
         {
             Config = config;
             if (!config.Token.IsNullOrEmpty()) Token = config.Token;
         }
+
         public Task<SwiftAuthV3Response> AuthenticateAsync()
             => AuthenticateAsync(Config.AuthUrl, Config.Name, Config.Password, Config.Domain);
 
 
         #region Account
+
         public Task<SwiftAccountDetailsResponse> AccountListContainersAsync()
             => AccountListContainersAsync(Config.ObjectStoreUrl);
 
         public Task<SwiftAccountDetailsResponse> AccountHeadAsync()
             => AccountHeadAsync(Config.ObjectStoreUrl);
 
-        public Task<SwiftBaseResponse> AccountPostAsync(Dictionary<string, string> metaValues = null, Dictionary<string, string> additionalHeaders = null)
+        public Task<SwiftBaseResponse> AccountPostAsync(Dictionary<string, string> metaValues = null,
+            Dictionary<string, string> additionalHeaders = null)
             => AccountPostAsync(Config.ObjectStoreUrl, metaValues, additionalHeaders);
 
         public Task<SwiftBaseResponse> AccountSetTempUrlKeyAsync(string key, bool firstKey = true)
             => AccountSetTempUrlKeyAsync(Config.ObjectStoreUrl, key, firstKey);
+
         #endregion Account
 
         #region Container
@@ -54,7 +60,8 @@ namespace NetSwiftClient
         public Task<SwiftContainerInfoResponse> ContainerHeadAsync(string container)
             => ContainerHeadAsync(Config.ObjectStoreUrl, container);
 
-        public Task<SwiftBaseResponse> ContainerPostAsync(string container, Dictionary<string, string> metaValues = null, Dictionary<string, string> additionalHeaders = null)
+        public Task<SwiftBaseResponse> ContainerPostAsync(string container,
+            Dictionary<string, string> metaValues = null, Dictionary<string, string> additionalHeaders = null)
             => ContainerPostAsync(Config.ObjectStoreUrl, container, metaValues, additionalHeaders);
 
         public Task<SwiftBaseResponse> ContainerPutAsync(string container, Dictionary<string, string> metadata = null)
@@ -64,17 +71,19 @@ namespace NetSwiftClient
 
         #region Object
 
-
         public Task<SwiftObjectGetResponse> ObjectGetAsync(string container, string objectName)
             => ObjectGetAsync(Config.ObjectStoreUrl, container, objectName);
 
-        public string ObjectGetTmpUrlAsync(string container, string objectName, TimeSpan expiresIn, string objectStoreKey, string fileName = null, string ipRange = null, bool? noDownloadButInline = null)
-            => ObjectGetTmpUrlAsync(Config.ObjectStoreUrl, container, objectName, expiresIn, objectStoreKey, fileName, ipRange, noDownloadButInline);
+        public string ObjectGetTmpUrlAsync(string container, string objectName, TimeSpan expiresIn,
+            string objectStoreKey, string fileName = null, string ipRange = null, bool? noDownloadButInline = null)
+            => ObjectGetTmpUrlAsync(Config.ObjectStoreUrl, container, objectName, expiresIn, objectStoreKey, fileName,
+                ipRange, noDownloadButInline);
 
         public Task<SwiftObjectGetResponse> ObjectHeadAsync(string container, string objectName)
             => base.ObjectHeadAsync(Config.ObjectStoreUrl, container, objectName);
 
-        public Task<SwiftBaseResponse> ObjectPostAsync(string container, string objectName, Dictionary<string, string> metaValues = null, Dictionary<string, string> additionalHeaders = null)
+        public Task<SwiftBaseResponse> ObjectPostAsync(string container, string objectName,
+            Dictionary<string, string> metaValues = null, Dictionary<string, string> additionalHeaders = null)
             => ObjectPostAsync(Config.ObjectStoreUrl, container, objectName, metaValues, additionalHeaders);
 
         public Task<SwiftBaseResponse> ObjectPutAsync(string container, string objectName, Stream data)
@@ -85,6 +94,7 @@ namespace NetSwiftClient
 
         public Task<SwiftBaseResponse> ObjectDeleteAsync(string container, string objectName)
             => ObjectDeleteAsync(Config.ObjectStoreUrl, container, objectName);
+
         #endregion Object
     }
 }
