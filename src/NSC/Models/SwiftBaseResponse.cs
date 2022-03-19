b8a9fd73a9283c9d72dc705f1065db2f76c10d53
serhiy.krasovskyy <xhunter74@gmail.com>
2022-03-19 17:14:51 +0200

    Fixed issue with decimal timestamp on headers.

--- src/NSC/Models/SwiftBaseResponse.cs
+++ src/NSC/Models/SwiftBaseResponse.cs
@@ -5,12 +5,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetSwiftClient.Models
 {
     public class SwiftBaseResponse
     {
+        private const string DefaultDecimalSeparator = ".";
+        private const string TimestampHeaderName = "X-Timestamp";
+
         public SwiftBaseResponse() { }
         public SwiftBaseResponse(HttpResponseMessage resp) { PrefillFromResponse(resp); }
 
@@ -47,6 +51,14 @@ namespace NetSwiftClient.Models
             Reason = resp.ReasonPhrase;
             StatusCode = resp.StatusCode;
 
+            var decimalSeparator = Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+
+            if (!decimalSeparator.Equals(DefaultDecimalSeparator) &&
+                Headers.ContainsKey(TimestampHeaderName) && Headers[TimestampHeaderName].Contains(DefaultDecimalSeparator))
+            {
+                Headers[TimestampHeaderName] = Headers[TimestampHeaderName].Replace(DefaultDecimalSeparator, decimalSeparator);
+            }
+
             foreach (var prop in this.GetType().GetProperties())
             {
                 object[] attrs = prop.GetCustomAttributes(true);
@@ -54,7 +66,7 @@ namespace NetSwiftClient.Models
                 {
                     if (!(attr is HasHeaderAttribute a)) continue;
                     if (Headers.Keys.Any(k => k.ToLower() == a.Name.ToLower()))
-                        prop.SetValue(this, Headers[Headers.Keys.First(h=>h.ToLower()==a.Name.ToLower())]
+                        prop.SetValue(this, Headers[Headers.Keys.First(h => h.ToLower() == a.Name.ToLower())]
                             .SafeConvert(prop.PropertyType, () => prop.PropertyType.IsValueType ? Activator.CreateInstance(prop.PropertyType) : null));
                 }
             }
